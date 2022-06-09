2d1fe570ea8f7096f17a1b5d18f2a663b9596080
Tether <tarun.sankhla.prc@gmail.com>
2022-06-09 14:14:05 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  module: {
+    loaders: [{
+      test:/\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }]
+  }
 }
 
 module.exports = nextConfig
